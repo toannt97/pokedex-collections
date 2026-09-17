1b0427aa08f0a519e4430828b2eb8c2a516a992a
toannt97 <nguyenthaitoan1997@gmail.com>
2026-09-17 12:06:56 +0700

    chore: config Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
@@ -10,4 +11,9 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./src/test/setup.ts"],
+    unstubGlobals: true,
+  },
 });

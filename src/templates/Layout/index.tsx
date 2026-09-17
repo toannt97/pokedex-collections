7acc4752e0c26eb6be6c3575eea5451caf65b9d9
toannt97 <nguyenthaitoan1997@gmail.com>
2026-09-17 10:47:10 +0700

    chore: adjust style and structure of Layout

--- src/templates/Layout/index.tsx
+++ src/templates/Layout/index.tsx
@@ -1,17 +1,31 @@
-import type { ReactNode } from "react";
+import { type ReactNode, useState } from "react";
 
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
+import { ScrollToTop } from "../../components/ScrollToTop";
+import { FloatingActionsContext } from "../../contexts/FloatingActionsContext";
 import styles from "./index.module.scss";
 
 type LayoutProps = {
   children: ReactNode;
 };
 
-export const Layout = ({ children }: LayoutProps) => (
-  <div className={styles.layout}>
-    <Header />
-    <main className={styles.layout__content}>{children}</main>
-    <Footer />
-  </div>
-);
+export const Layout = ({ children }: LayoutProps) => {
+  const [floatingActionsSlot, setFloatingActionsSlot] = useState<HTMLDivElement | null>(null);
+
+  return (
+    <FloatingActionsContext value={floatingActionsSlot}>
+      <div className={styles.layout}>
+        <Header />
+        <main className={styles.layout__content}>{children}</main>
+        <div className={styles["layout__floating-actions"]}>
+          <div ref={setFloatingActionsSlot} className={styles["layout__floating-slot"]} />
+          <div className={styles["layout__scroll-to-top"]}>
+            <ScrollToTop />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </FloatingActionsContext>
+  );
+};
